69262c7dd6a2cf31e9fad08891635ff9b69bf727
Rohan-Devlogs <rohan@devlogs.com>
2025-09-26 20:35:39 +0530

    Demo-submit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/Header'
+import About from './pages/about'
+import Testimonial from './pages/Testimonial'
+import Contact from './pages/contact'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <About/>
+    <Testimonial/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header>
+      <h1>This is a Header</h1>
+    </header>
+    </>
+  )
+}
+
+export default Header
